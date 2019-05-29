4333dd0a6ce18fc301124e000feb4fd27f4d0591
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-29 16:45:16 +0100

    Correct output references for chain role

--- gds_chain_role/output.tf
+++ gds_chain_role/output.tf
@@ -1,11 +1,11 @@
 output "role_id" {
-  value = "${aws_iam_role.gds_security_audit_role.id}"
+  value = "${aws_iam_role.gds_chain_role.id}"
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.gds_security_audit_role.arn}"
+  value = "${aws_iam_role.gds_chain_role.arn}"
 }
 
 output "policy_id" {
-  value = "${aws_iam_role_policy.gds_security_audit_role_policy.id}"
+  value = "${aws_iam_role_policy.gds_chain_role_policy.id}"
 }
