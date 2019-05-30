e04e8d5b2b8539c556351b949fb4c19a17014ec1
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-30 09:36:12 +0100

    Try building the trust using an aws_iam_policy_document data module

--- gds_chain_role/gds_chain_role.tf
+++ gds_chain_role/gds_chain_role.tf
@@ -1,15 +1,28 @@
-data "template_file" "trust" {
-  template = "${file("${path.module}/json/trust.json")}"
+#data "template_file" "trust" {
+#  template = "${file("${path.module}/json/trust.json")}"
+#
+#  vars {
+#    trust_arns  = "${jsonencode(var.trust_arns)}"
+#  }
+#}
 
-  vars {
-    trust_arns  = "${jsonencode(var.trust_arns)}"
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["${var.trust_arns}"]
+    }
   }
 }
 
 resource "aws_iam_role" "gds_chain_role" {
   name = "${var.role_name}"
 
-  assume_role_policy = "${data.template_file.trust.rendered}"
+  #assume_role_policy = "${data.template_file.trust.rendered}"
+  assume_role_policy = "${data.aws_iam_policy_document.trust.json}"
 }
 
 
