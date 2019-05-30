86de83507c1d03424a3190d28a55b3931748c79f
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-30 09:40:49 +0100

    Reimplement role policy using aws_iam_policy_document data model in place of json template

--- gds_chain_role/policy.tf
+++ gds_chain_role/policy.tf
@@ -1,15 +1,24 @@
 data "aws_caller_identity" "current" {}
 
-data "template_file" "policy" {
-  template = "${file("${path.module}/json/policy.json")}"
+#data "template_file" "policy" {
+#  template = "${file("${path.module}/json/policy.json")}"
+#
+#  vars {
+#    assume_arn = "${var.assume_arn}"
+#  }
+#}
 
-  vars {
-    assume_arn = "${var.assume_arn}"
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = "${var.assume_arn}"
   }
 }
 
 resource "aws_iam_role_policy" "gds_chain_role_policy" {
   name    = "${var.role_name}Policy"
   role    = "${aws_iam_role.gds_chain_role.id}"
-  policy  = "${data.template_file.policy.rendered}"
+#  policy  = "${data.template_file.policy.rendered}"
+  policy  = "${data.aws_iam_policy_document.policy.json}"
 }
