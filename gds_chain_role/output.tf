93223e587e2fb46a48e92e7b07b3ab50381afe2c
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-30 10:06:22 +0100

    Add role name to module outputs and remove json template policy files

--- gds_chain_role/output.tf
+++ gds_chain_role/output.tf
@@ -6,6 +6,10 @@ output "role_arn" {
   value = "${aws_iam_role.gds_chain_role.arn}"
 }
 
+output "role_name" {
+  value = "${aws_iam_role.gds_chain_role.name}"
+}
+
 output "policy_id" {
   value = "${aws_iam_role_policy.gds_chain_role_policy.id}"
 }
--- gds_security_audit_role/output.tf
+++ gds_security_audit_role/output.tf
@@ -5,3 +5,7 @@ output "role_id" {
 output "role_arn" {
   value = "${aws_iam_role.gds_security_audit_role.arn}"
 }
+
+output "role_name" {
+  value = "${aws_iam_role.gds_security_audit_role.name}"
+}
