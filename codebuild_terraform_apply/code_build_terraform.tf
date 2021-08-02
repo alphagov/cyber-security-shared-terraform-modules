7482802fefb752c43f3be9551521d0d96348e438
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-02 16:57:17 +0100

    Remove unused registry_credential block

--- codebuild_terraform_apply/code_build_terraform.tf
+++ codebuild_terraform_apply/code_build_terraform.tf
@@ -20,11 +20,6 @@ resource "aws_codebuild_project" "code_pipeline_terraform" {
     image_pull_credentials_type = "SERVICE_ROLE"
     privileged_mode             = false
 
-    registry_credential {
-      credential          = data.aws_secretsmanager_secret.dockerhub_creds.arn
-      credential_provider = "SECRETS_MANAGER"
-    }
-
     environment_variable {
       name  = "AWS_ACCOUNT_ID"
       value = var.deployment_account_id
