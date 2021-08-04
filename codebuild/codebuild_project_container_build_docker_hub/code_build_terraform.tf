9a752cb13c987835bf625e49575e2d600b62c170
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-04 16:52:24 +0100

    Fixup variable name mismatch

--- codebuild/codebuild_project_container_build_docker_hub/code_build_terraform.tf
+++ codebuild/codebuild_project_container_build_docker_hub/code_build_terraform.tf
@@ -2,7 +2,7 @@ resource "aws_codebuild_project" "code_pipeline_container_build_docker_hub" {
   name        = "${var.pipeline_name}-dockerhub-deploy-${var.environment}"
   description = "Build and push images to dockerhub"
 
-  service_role = var.service_role_arn
+  service_role = var.service_role_name
 
   artifacts {
     type = "CODEPIPELINE"
