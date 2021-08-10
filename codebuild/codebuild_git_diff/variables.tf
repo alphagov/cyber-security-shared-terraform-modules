2cf1d90d3ef1a939b879153fbb4057e4de96ca2f
alice-carr <alice.bc.carr@gmail.com>
2021-08-10 14:16:33 +0100

    add dockerhb credentials as var

--- codebuild/codebuild_git_diff/variables.tf
+++ codebuild/codebuild_git_diff/variables.tf
@@ -53,3 +53,8 @@ variable "codebuild_image" {
   description = ""
   type        = string
 }
+
+variable "docker_hub_credentials" {
+  description = "The name of the Secrets Manager secret that contains the username and password for the Docker Hub"
+  type        = string
+}
