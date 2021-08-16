8f7a329a9cf8d7f0846f47e13d70af2a68cfb2d3
alice-carr <alice.bc.carr@gmail.com>
2021-08-16 16:09:28 +0100

    revert to small compute size

--- codebuild/codebuild_git_diff/code_build_git_diff.tf
+++ codebuild/codebuild_git_diff/code_build_git_diff.tf
@@ -21,7 +21,7 @@ resource "aws_codebuild_project" "code_pipeline_git_diff" {
 
 
   environment {
-    compute_type                = "BUILD_GENERAL1_2XLARGE"
+    compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = var.codebuild_image
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
