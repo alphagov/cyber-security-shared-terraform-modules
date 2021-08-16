ffcbb10da38866057005ea6032163d976b78ef8d
alice-carr <alice.bc.carr@gmail.com>
2021-08-16 16:07:20 +0100

    upgrade compute type

--- codebuild/codebuild_git_diff/code_build_git_diff.tf
+++ codebuild/codebuild_git_diff/code_build_git_diff.tf
@@ -21,7 +21,7 @@ resource "aws_codebuild_project" "code_pipeline_git_diff" {
 
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
+    compute_type                = "BUILD_GENERAL1_2XLARGE"
     image                       = var.codebuild_image
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
