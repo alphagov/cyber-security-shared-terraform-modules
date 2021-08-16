5c0f120314d43df4fe446da1fed066fac398606d
gds-ahine <aaron.hine@digital.cabinet-office.gov.uk>
2021-08-16 15:03:25 +0100

    WIP - add Python tox codebuild module

---
+++ codebuild/codebuild_python_tox/code_build_python_tox.tf
@@ -0,0 +1,48 @@
+resource "aws_codebuild_project" "code_pipeline_python_tox" {
+  name        = "${var.pipeline_name}-python-tox-${var.environment}"
+  description = "Run Python tox"
+
+  service_role = data.aws_iam_role.execution_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = var.codebuild_image
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    privileged_mode             = false
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = var.deployment_account_id
+    }
+
+    environment_variable {
+      name  = "PYTHON_SOURCE_DIRECTORY"
+      value = ""
+    }
+
+    environment_variable {
+      name  = "PYTHON_VERSION"
+      value = ""
+    }
+
+    environment_variable {
+      name  = "ROLE_NAME"
+      value = var.deployment_role_name
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("${path.module}/code_build_python_tox.yml")
+  }
+}
---
+++ codebuild/codebuild_python_tox/variables.tf
@@ -0,0 +1,35 @@
+variable "region_name" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "deployment_account_id" {
+  description = "the account into which the terraform will be deployed"
+  type        = string
+}
+
+variable "deployment_role_name" {
+  description = "the role used to deploy the terraform"
+  type        = string
+}
+
+
+variable "codebuild_service_role_name" {
+  description = "the role code build uses to access other AWS services"
+  type        = string
+}
+
+variable "codebuild_image" {
+  description = "The image that CodeBuild will use, including the tag."
+  type        = string
+}
+
+variable "pipeline_name" {
+  description = "The name of the pipeline this project will be a part of"
+  type        = string
+}
+
+variable "environment" {
+  description = "e.g. staging, production"
+  type        = string
+}
