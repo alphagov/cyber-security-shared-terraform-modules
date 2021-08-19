aadc6114b02763a788734930074be85be19b599b
gds-ahine <aaron.hine@digital.cabinet-office.gov.uk>
2021-08-19 10:29:40 +0100

    wip - testing ECR shared module

--- codebuild/codebuild_ECR_push/outputs.tf
+++ codebuild/codebuild_ECR_push/outputs.tf
@@ -1,3 +1,6 @@
-# output "project_name" {
-#   value = aws_codebuild_project.code_pipeline_ecr_container[each.key].name
-# }
+output "project_name" {
+  #value = aws_codebuild_project.code_pipeline_ecr_container[each.key].name
+  value = {
+    for k, v in aws_codebuild_project.code_pipeline_ecr_container : k => v.name
+  }
+}
