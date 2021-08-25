69ddc442871c21707d91660cf858b3f124ea72d1
alice-carr <alice.bc.carr@gmail.com>
2021-08-25 16:26:38 +0100

    create new module for get actions required

---
+++ codebuild/codebuild_get_actions_required/codebuild_get_actions_required.tf
@@ -0,0 +1,48 @@
+resource "aws_codebuild_project" "code_pipeline_get_actions_required" {
+  name        = "${var.pipeline_name}-get-actions-required-${var.environment}"
+  description = "Reads changed_files.json and actions_triggers.json to produce actions_required.json."
+
+  service_role = data.aws_iam_role.execution_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  secondary_artifacts {
+    type                = "S3"
+    name                = "actions_required.json"
+    artifact_identifier = "rebuild_task"
+    location            = var.artifact_bucket
+    path                = var.output_artifact_path 
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  }
+
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = var.codebuild_image
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    privileged_mode             = false
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = var.deployment_account_id
+    }
+
+    environment_variable {
+      name  = "ROLE_NAME"
+      value = var.deployment_role_name
+    }
+
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("${path.module}/codebuild_get_actions_required.yml")
+  }
+}
---
+++ codebuild/codebuild_get_actions_required/variables.tf
@@ -0,0 +1,44 @@
+variable "environment" {
+  type        = string
+  description = "Enviroment this module will be ran"
+}
+
+variable "pipeline_name" {
+  type        = string
+  description = "Name of the pipeline"
+}
+
+variable "deployment_account_id" {
+  description = "The AWS account id where you are deploying to ECR image to."
+  type        = string
+}
+
+variable "deployment_role_name" {
+  description = "The name of the role used to deploy the ECR image."
+  type        = string
+}
+
+variable "codebuild_service_role_name" {
+  description = "The role code build uses to access other AWS services."
+  type        = string
+}
+
+variable "artifact_bucket" {
+  description = "S3 path where the artifact will be stored."
+  type        = string
+}
+
+variable "output_artifact_path" {
+  description = "the S3 path to store the output atrifact"
+  type        = string
+}
+
+variable "codebuild_image" {
+  description = "the image in which you want to run the codebuild task"
+  type        = string
+}
+
+variable "action_triggers" {
+    description = "the path to the action_triggers.json file in your repo"
+    type        = "string"
+}
