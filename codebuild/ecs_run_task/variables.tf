e7c98d0fe70f7c781230ac0ce127cf309c4ac3b1
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-09-01 18:37:41 +0100

    Add missing variables

--- codebuild/ecs_run_task/variables.tf
+++ codebuild/ecs_run_task/variables.tf
@@ -8,6 +8,11 @@ variable environment {
   type        = string
 }
 
+variable "codebuild_service_role_name" {
+  description = "the role code build uses to access other AWS services"
+  type        = string
+}
+
 variable deployment_account_id {
   description = "the account into which the terraform will be deployed"
   type        = string
@@ -15,6 +20,16 @@ variable deployment_account_id {
 
 variable deployment_role_name {
   description = "the role used to deploy the terraform"
+  type        = string
+}
+
+variable "docker_hub_credentials" {
+  description = "The name of the Secrets Manager secret that contains the username and password for the Docker Hub"
+  type        = string
+}
+
+variable "codebuild_image" {
+  description = "The image that CodeBuild will use, including the tag."
   type        = string
 }
 
