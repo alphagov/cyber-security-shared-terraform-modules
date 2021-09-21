989ab6803b402ebc6b893af77d897f7bcf6bb483
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-09-21 10:40:14 +0100

    Shorten name of event rule
    Was causing issues with long pipeline names since an event rule can only
    have 64 characters in its name.

--- cloudwatch/cloudwatch_report_codepipeline_status/cloudwatch.tf
+++ cloudwatch/cloudwatch_report_codepipeline_status/cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_rule" "codepipeline_event_rule" {
-  name        = "codepipeline-${var.pipeline_name}-healthcheck-event-rule"
+  name        = "codepipeline-events-${var.pipeline_name}"
   description = "Codepipeline Execution State Change"
 
   event_pattern = <<EOF
